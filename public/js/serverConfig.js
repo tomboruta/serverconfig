300130b57545bb9d7f7776b507ecaa149d7b6d8a
Tom Boruta <tomb@teamddm.com>
2014-04-23 14:09:51 -0400

    initial commit

---
+++ public/js/serverConfig.js
@@ -0,0 +1,214 @@
+/**
+ServerConfig.io
+Author: Tom Boruta
+http://tomboruta.com
+**/
+serverConfig = [];
+
+function setDefaults(){
+	serverConfig = {
+		"memoryButtons" : [
+			{
+				"buttonText":"512MB",
+				"multiplier":0.5
+			},
+			{
+				"_comment":"This is the default server size.",
+				"buttonText":"1GB",
+				"multiplier":1
+			},
+			{
+				"buttonText":"2GB",
+				"multiplier":2
+			},
+			{
+				"buttonText":"4GB",
+				"multiplier":4
+			},
+			{
+				"buttonText":"8GB",
+				"multiplier":8
+			}
+		],
+		"apache2Settings" : [
+			{
+				"field":"StartServers",
+				"val":2,
+				"multiplierModifier":1
+			},
+			{
+				"field":"MinSpareServers",
+				"val":6,
+				"multiplierModifier":1
+			},
+			{
+				"field":"MaxSpareServers",
+				"val":12,
+				"multiplierModifier":1
+			},
+			{
+				"field":"MaxClients",
+				"val":40,
+				"multiplierModifier":1
+			},
+			{
+				"field":"MaxRequestsPerChild",
+				"val":300,
+				"multiplierModifier":false
+			}
+		],
+		"phpSettings" : [
+			{
+				"field":"max_execution_time",
+				"val":30,
+				"multiplierModifier":false
+			},
+			{
+				"field":"memory_limit",
+				"val":128000000,
+				"multiplierModifier":false
+			}
+		],
+		"myisamSettings" : [
+			{
+				"field":"max_connections",
+				"val":80,
+				"multiplierModifier":1
+			},
+			{
+				"field":"key_buffer",
+				"val":32000000,
+				"multiplierModifier":1
+			},
+			{
+				"field":"max_allowed_packet",
+				"val":1000000,
+				"multiplierModifier":1
+			},
+			{
+				"field":"thread_stack",
+				"val":128000,
+				"multiplierModifier":1
+			},
+			{
+				"field":"table_cache",
+				"val":32,
+				"multiplierModifier":1
+			}
+		]
+	};
+}
+
+function displayMemoryButtons(){
+	var htmloutput='';
+	$.each(serverConfig.memoryButtons, function(i, item){
+		htmloutput +='<button type="button" class="btn btn-lg btn-default" data-multiplier="'+item.multiplier+'">'+item.buttonText+'</button> ';
+	});
+	$('#memoryButtons').html(htmloutput);
+	$('button[data-multiplier=1]').addClass('active');
+}
+
+function displayInputs(settings,spanId){
+	var htmloutput='';
+	$.each(settings, function(i, item){
+		htmloutput += '<div class="form-group"><label for="'+item.field+'" class="col-md-7 control-label">'+item.field+'</label><div class="col-md-5"><input type="text" class="form-control" id="'+item.field+'" value="-" readonly></div></div>';
+	});
+	$('#'+spanId).html(htmloutput);
+}
+
+function abbrNum(number, decPlaces) {
+    decPlaces = Math.pow(10,decPlaces);
+    var abbrev = [ "K", "M", "G", "T" ];
+    for (var i=abbrev.length-1; i>=0; i--) {
+        var size = Math.pow(10,(i+1)*3);
+        if(size <= number) {
+             number = Math.round(number*decPlaces/size)/decPlaces;
+             if((number == 1000) && (i < abbrev.length - 1)) {
+                 number = 1;
+                 i++;
+             }
+             number += abbrev[i];
+             break;
+        }
+    }
+    return number;
+}
+
+function shortenNumbers(){
+	$('input').each(function(i,item){
+		if ($(item).val()>9999){
+			$(item).val(abbrNum($(item).val(), 0));
+		}
+	});
+}
+
+function redisplayInputValues(settings){
+	$.each(settings, function(i, item){
+		$('#'+item.field).val(item.val);
+	});
+}
+
+function redisplaySettings(settings,divId){
+	$('#'+divId).html(function(){
+		htmloutput='';
+		$.each(settings, function(i, item){
+			htmloutput += item.field + ' ';
+			htmloutput += $('#'+item.field).val() + '<br>';
+		});
+		return htmloutput;
+	});
+}
+
+function redisplayNumbers(){
+	redisplayInputValues(serverConfig.apache2Settings);
+	redisplayInputValues(serverConfig.phpSettings);
+	redisplayInputValues(serverConfig.myisamSettings);
+
+	shortenNumbers();
+
+	redisplaySettings(serverConfig.apache2Settings,'apache2Settings');
+	redisplaySettings(serverConfig.phpSettings,'phpSettings');
+	redisplaySettings(serverConfig.myisamSettings,'myisamSettings');
+}
+
+function recalculateNumbers(multiplier){
+	setDefaults();
+	$.each(serverConfig.apache2Settings, function(i, item){
+		if (item.multiplierModifier){
+			item.val=item.val*(multiplier*item.multiplierModifier);
+		}
+	});
+	$.each(serverConfig.myisamSettings, function(i, item){
+		if (item.multiplierModifier){
+			item.val=item.val*(multiplier*item.multiplierModifier);
+		}
+	});
+	redisplayNumbers();
+}
+
+$(document).ready(function() {
+
+	setDefaults();
+
+	displayMemoryButtons();
+	displayInputs(serverConfig.apache2Settings,'apache2Inputs');
+	displayInputs(serverConfig.phpSettings,'phpInputs');
+	displayInputs(serverConfig.myisamSettings,'myisamInputs');
+
+	redisplayNumbers();
+
+	// selecting a memory size
+	$('.server-selection button').on('click',function(){
+		if (!$(this).hasClass('active')){
+			recalculateNumbers($(this).data('multiplier'));
+		}
+		$(this).addClass('active');
+		$(this).parent().children('.server-selection button').not(this).removeClass('active');
+		$('.displayServerSize').html($(this).html());
+	});
+	$('.os-selection button').on('click',function(){
+		$(this).addClass('active');
+		$(this).parent().children('.os-selection button').not(this).removeClass('active');
+		$('.displayOS').html($(this).html());
+	});
+});
---
+++ readme.md
@@ -0,0 +1,46 @@
+## Server Config
+
+[Server Config](http://serverconfig.io/) is a project to help web developers use sensible server settings for web technologies. 
+
+### Contriubting
+
+Given all the various web technologies and nuances in those technologies I have added this project to Github in the hopes to get the best info and the best settings for each technology. 
+
+Note that this is not the full website. I have written the actual website in the php framework Laravel. However, the core functionality (currently) of Server Config does not need Laravel, so I did not want to hinder those without knowlegde of that particular framework from being discouraged from contributing. To that end, there is no need to make you setup a Laravel project to contribute to this repo.
+
+I've tried to make it as easy as possible for anyone with HTML, jQuery, and Javascript knowledge to contribute. Fork this repo, set it up on your machine and open /public/index.html in your web browser to test. All the functionality is in /public/js/serverConfig.js
+
+Issues and Pull Requests are always welcome.
+
+### To Do
+
+- Add pop-up descriptions for each setting
+- Add more important settings
+- Add "best practices" settings
+- Webservers to add
+	- Nginx
+- Languages to add
+	- Python
+	- Ruby
+	- Node.js
+- Datastores to add
+	- MySQL Innodb
+	- MongoDB
+	- Postgres
+- Add more OSes
+	- Change configuration file locations for each OS
+- Add better defaults for CMSes
+	- Wordpress
+	- Drupal
+- Add caches (I'm not sure how to calculate the memory impact of these...)
+	- Memcache
+	- Varnish
+- Advanced Calculator
+	- Allow users to change individual values and recommend a server size
+- Let users separate the technologies to different servers
+- Downloadable script to change the settings on a server (https)
+- Look into using APIs to generate servers based on values in the calculator
+	- Digital Ocean
+	- Linode
+	- Google Cloud
+	- AWS
