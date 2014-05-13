99afd569bf4bc009a653f697cf99e3f3500eaca0
Tom Boruta <tomb@teamddm.com>
2014-05-13 14:05:10 -0400

    Add AdminLTE theme

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -7,15 +7,32 @@
 			@show 
 		</title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+		<!-- bootstrap 3.0.2 -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<!-- font Awesome -->
+		<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		<!-- Ionicons -->
+		<link href="css/ionicons.min.css" rel="stylesheet" type="text/css" />
+		<!-- Morris chart -->
+		<link href="css/morris/morris.css" rel="stylesheet" type="text/css" />
+		<!-- jvectormap -->
+		<link href="css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+		<!-- fullCalendar -->
+		<link href="css/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
+		<!-- Daterange picker -->
+		<link href="css/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+		<!-- bootstrap wysihtml5 - text editor -->
+		<link href="css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+		<!-- Theme style -->
+		<link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
 
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/main.css">
-
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-		<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+          <![endif]-->
 	</head>
 
 	<body>
@@ -68,8 +85,40 @@
 
 		<!-- Javascripts
 		================================================== -->
-		<script src="js/jquery-2.0.2.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<!-- jQuery 2.0.2 -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<!-- jQuery UI 1.10.3 -->
+		<script src="js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+		<!-- Bootstrap -->
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<!-- Morris.js charts -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+		<script src="js/plugins/morris/morris.min.js" type="text/javascript"></script>
+		<!-- Sparkline -->
+		<script src="js/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+		<!-- jvectormap -->
+		<script src="js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
+		<script src="js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+		<!-- fullCalendar -->
+		<script src="js/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+		<!-- jQuery Knob Chart -->
+		<script src="js/plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
+		<!-- daterangepicker -->
+		<script src="js/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+		<!-- Bootstrap WYSIHTML5 -->
+		<script src="js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+		<!-- iCheck -->
+		<script src="js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+
+		<!-- AdminLTE App -->
+		<script src="js/AdminLTE/app.js" type="text/javascript"></script>
+		
+		<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+		<script src="js/AdminLTE/dashboard.js" type="text/javascript"></script>     
+		
+		<!-- AdminLTE for demo purposes -->
+		<script src="js/AdminLTE/demo.js" type="text/javascript"></script>
+        
 		<script src="/js/serverConfig.js"></script>
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
